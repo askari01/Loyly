49efd1f580de45f5d5e2ccff181e53612a122d7b
askari01 <farrukh.askari01@gmail.com>
2017-07-06 17:42:14 +0500

    initial commit Loyly app

---
+++ Loyly/EssentialOilsVC.swift
@@ -0,0 +1,52 @@
+//
+//  EssentialOilsVC.swift
+//  Loyly
+//
+//  Created by Syed Askari on 7/6/17.
+//  Copyright © 2017 Syed Askari. All rights reserved.
+//
+
+import UIKit
+
+class EssentialOilsVC: UIViewController,UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        return cell
+    }
+    
+}
